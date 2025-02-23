4db787579eddef6f12196fa7f69241efef2f14ce
Rebakure <kevinrebakure@gmail.com>
2025-02-23 17:45:14 +0200

    Keep the container inside App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import AboutMe from "./components/layout/AboutMe";
@@ -15,18 +15,20 @@ export default function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <Box
-        sx={{
-          backgroundImage:
-            "url('https://img.freepik.com/free-photo/abstract-textured-backgound_1258-30471.jpg?t=st=1740325129~exp=1740328729~hmac=6af6cabd4bdfa231bb7673cf7085d22239eb86e365babd75c3eb4a4ce8c2aed2&w=900')",
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-        }}
-      >
-        <NavBar />
-        <HeroSection />
-      </Box>
-      <AboutMe />
+      <Container>
+        <Box
+          sx={{
+            backgroundImage:
+              "url('https://img.freepik.com/free-photo/abstract-textured-backgound_1258-30471.jpg?t=st=1740325129~exp=1740328729~hmac=6af6cabd4bdfa231bb7673cf7085d22239eb86e365babd75c3eb4a4ce8c2aed2&w=900')",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
+        >
+          <NavBar />
+          <HeroSection />
+        </Box>
+        <AboutMe />
+      </Container>
     </ThemeProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,9 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Container } from "@mui/material";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Container>
       <App />
-    </Container>
   </StrictMode>,
 );
