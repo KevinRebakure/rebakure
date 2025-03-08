44ad337dea7ae103cfa541eb641822e7c8ade279
Rebakure <kevinrebakure@gmail.com>
2025-03-08 20:39:52 +0200

    Create user icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,36 @@
+import {
+  BulbOutlined,
+  FolderOutlined,
+  TagOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Flex, Layout } from "antd";
+
+const { Content, Footer, Header } = Layout;
+
 export default function App() {
-  return <p>basda</p>;
+  return (
+    <Layout>
+      <Header>
+        <Flex
+          gap="large"
+          justify="center"
+          align="center"
+          style={{ border: "1px solid white", height: "100%" }}
+        >
+          <UserOutlined style={iconStyles} />
+          <FolderOutlined style={iconStyles} />
+          <BulbOutlined style={iconStyles} />
+          <TagOutlined style={iconStyles} />
+        </Flex>
+      </Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  );
 }
+
+const iconStyles: React.CSSProperties = {
+  color: "white",
+  fontSize: 28,
+};
