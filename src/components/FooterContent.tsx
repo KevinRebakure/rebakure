25fd8fa0366b07659ebcecbe0cc423c54635f594
Rebakure <kevinrebakure@gmail.com>
2025-03-24 21:20:42 +0200

    Use flex component for the footer layout

--- src/components/FooterContent.tsx
+++ src/components/FooterContent.tsx
@@ -10,52 +10,50 @@ function FooterContent() {
     <>
       <Row>
         <Col span={6}>
-          <Flex vertical>
-            <Title level={3}>rebakure.com</Title>
-            <Text>Let's learn and build stuff together.</Text>
+          <Flex vertical justify="space-between" style={{ height: "100%" }}>
+            <Flex vertical>
+              <Title level={3}>rebakure.com</Title>
+              <Text>Let's learn and build stuff together.</Text>
+            </Flex>
+
+            <Flex vertical gap="large">
+              <Space size="middle" align="center">
+                <GitHubIcon style={{ fontSize: 30 }} />
+                <FontAwesomeIcon
+                  icon={faLinkedin}
+                  style={{ width: 30, height: 30 }}
+                />
+                <XIcon style={{ fontSize: 30 }} />
+                <InstagramIcon style={{ fontSize: 30 }} />
+              </Space>
+              <Text underline>&copy;2025 rebakure.com</Text>
+            </Flex>
           </Flex>
         </Col>
-
         <Col span={6}>
-          <Flex vertical gap="middle">
-            <Title level={4}>My Portfolio</Title>
-            <Text>My projects collection</Text>
-            <Text>Contribute to my projects</Text>
+          <Flex vertical gap={150}>
+            <Flex vertical gap="middle">
+              <Title level={4}>My Portfolio</Title>
+              <Text>My projects collection</Text>
+              <Text>Contribute to my projects</Text>
+            </Flex>
+
+            <Flex vertical gap="large">
+              <Title level={4}>Open to the community</Title>
+              <Space>
+                <Text>Be a core member</Text>
+                <Tag color="green">Upcoming</Tag>
+              </Space>
+              <Space>
+                <Text>Share your projects</Text>
+                <Tag color="green">Upcoming</Tag>
+              </Space>
+              <Text underline>Credits</Text>
+            </Flex>
           </Flex>
         </Col>
-
         <Col span={12}>
           <EmailMeForm />
-        </Col>
-      </Row>
-      <Row>
-        <Col span={6}>
-          <Flex vertical gap="middle">
-            <Space size="middle" align="center">
-              <GitHubIcon style={{ fontSize: 30 }} />
-              <FontAwesomeIcon
-                icon={faLinkedin}
-                style={{ width: 30, height: 30 }}
-              />
-              <XIcon style={{ fontSize: 30 }} />
-              <InstagramIcon style={{ fontSize: 30 }} />
-            </Space>
-            <Text underline>&copy;2025 rebakure.com</Text>
-          </Flex>
-        </Col>
-        <Col span={6}>
-          <Flex vertical gap="middle">
-            <Title level={4}>Open to the community</Title>
-            <Space>
-              <Text>Be a core member</Text>
-              <Tag color="green">Upcoming</Tag>
-            </Space>
-            <Space>
-              <Text>Share your projects</Text>
-              <Tag color="green">Upcoming</Tag>
-            </Space>
-            <Text underline>Credits</Text>
-          </Flex>
         </Col>
       </Row>
     </>
