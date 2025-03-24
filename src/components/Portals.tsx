c76adf965def376075f4b42b15b7486ed84ccdb3
Rebakure <kevinrebakure@gmail.com>
2025-03-24 23:50:26 +0200

    Make portals responsive

--- src/components/Portals.tsx
+++ src/components/Portals.tsx
@@ -2,21 +2,27 @@ import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Card, Flex, Typography } from "antd";
 import { GitHubIcon, GmailIcon, InstagramIcon, XIcon } from "../assets/icons";
+import { Grid } from "antd";
 
 const { Text, Title } = Typography;
+const { useBreakpoint } = Grid;
 
 function Portals() {
+  const screens = useBreakpoint();
   return (
     <Card
       style={{
-        width: "70%",
         margin: "0 auto",
       }}
+      className="w-[90%] sm:w-[70%]"
     >
       <Title level={3} style={{ marginBottom: 30 }}>
         Let's connect...
       </Title>
-      <Flex justify="center" gap={100}>
+      <Flex
+        className="justify-between sm:justify-center"
+        gap={screens.md ? 100 : "large"}
+      >
         {portals.map((portal) => (
           <Flex
             key={portal.platform}
@@ -25,7 +31,8 @@ function Portals() {
             gap="small"
             style={{ width: "max-content" }}
           >
-            {portal.icon} <Text>{portal.username}</Text>
+            {portal.icon}{" "}
+            <Text className="hidden sm:block">{portal.username}</Text>
           </Flex>
         ))}
       </Flex>
