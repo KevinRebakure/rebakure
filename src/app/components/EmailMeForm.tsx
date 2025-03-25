2589d9ed60169e1115702f0069c5d70601358b60
Rebakure <kevinrebakure@gmail.com>
2025-03-26 00:10:02 +0200

    Log the rendered email after form submission

--- src/app/components/EmailMeForm.tsx
+++ src/app/components/EmailMeForm.tsx
@@ -1,11 +1,27 @@
-"use client";
-
 import { Button, Form, FormProps, Input, Space, Typography } from "antd";
 import SendIcon from "../assets/icons/SendIcon";
+import renderEmail from "../utils/RenderEmail";
 const { Title } = Typography;
 
 function EmailMeForm() {
-  const [form] = Form.useForm();
+  const [form] = Form.useForm<FieldType>();
+
+  const onFinish: FormProps<FieldType>["onFinish"] = (values: FieldType) => {
+    if (values) {
+      renderEmail({
+        names: values.fullName!,
+        message: values.message!,
+      }).then((message) => console.log(message));
+
+      // console.log("Success:", values);
+    }
+  };
+
+  const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (
+    errorInfo
+  ) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
     <Form
       name="Email me form"
@@ -60,14 +76,6 @@ function EmailMeForm() {
     </Form>
   );
 }
-
-const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
-  console.log("Success:", values);
-};
-
-const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
-  console.log("Failed:", errorInfo);
-};
 
 interface FieldType {
   fullName?: string;
