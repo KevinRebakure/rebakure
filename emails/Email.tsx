29e1f0ac01ab40ccde011a9ee4ef97124c5ba6d8
Rebakure <kevinrebakure@gmail.com>
2025-03-25 23:31:05 +0200

    A simple email

--- emails/Email.tsx
+++ emails/Email.tsx
@@ -1,15 +1,26 @@
-import { Button, Html } from "@react-email/components";
-import * as React from "react";
+import { Heading, Html, Text } from "@react-email/components";
 
-export default function Email() {
+interface Props {
+  names: string;
+}
+
+export default function Email({ names }: Props) {
   return (
     <Html>
-      <Button
-        href="https://example.com"
-        style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
-      >
-        Click me
-      </Button>
+      <Heading style={{ textAlign: "center" }} as="h1">
+        {names} reaching out!
+      </Heading>
+      <Text style={{ fontWeight: "bold" }}>Full names:</Text>
+      <Text>{names}</Text>
+      <Text style={{ width: "50%" }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut eros
+        leo. Nam vel elit commodo, pretium justo nec, mattis arcu. Aenean
+        faucibus est at massa tincidunt, ut suscipit mi fermentum. Aenean
+        commodo lacus enim, vitae auctor lacus dictum at. In augue metus,
+        pellentesque quis varius ac, faucibus consectetur justo. Phasellus lacus
+        libero, lacinia in auctor vel, cursus nec justo. Ut quis malesuada nisi,
+        ut vulputate neque.
+      </Text>
     </Html>
   );
 }
