d7367a20d4e7a2220d6172244562abe6814f1d47
Rebakure <kevinrebakure@gmail.com>
2025-03-26 00:08:35 +0200

    Sample email

--- emails/Email.tsx
+++ emails/Email.tsx
@@ -2,9 +2,10 @@ import { Heading, Html, Text } from "@react-email/components";
 
 interface Props {
   names: string;
+  message: string;
 }
 
-export default function Email({ names }: Props) {
+export default function Email({ names, message }: Props) {
   return (
     <Html>
       <Heading style={{ textAlign: "center" }} as="h1">
@@ -12,15 +13,7 @@ export default function Email({ names }: Props) {
       </Heading>
       <Text style={{ fontWeight: "bold" }}>Full names:</Text>
       <Text>{names}</Text>
-      <Text style={{ width: "50%" }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut eros
-        leo. Nam vel elit commodo, pretium justo nec, mattis arcu. Aenean
-        faucibus est at massa tincidunt, ut suscipit mi fermentum. Aenean
-        commodo lacus enim, vitae auctor lacus dictum at. In augue metus,
-        pellentesque quis varius ac, faucibus consectetur justo. Phasellus lacus
-        libero, lacinia in auctor vel, cursus nec justo. Ut quis malesuada nisi,
-        ut vulputate neque.
-      </Text>
+      <Text style={{ width: "50%" }}>{message}</Text>
     </Html>
   );
 }
