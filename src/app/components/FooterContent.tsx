9ab9c61c959eb005c991a30eacb14822e1574825
Rebakure <kevinrebakure@gmail.com>
2025-03-30 13:42:38 +0200

    Remove send me an email form

--- src/app/components/FooterContent.tsx
+++ src/app/components/FooterContent.tsx
@@ -1,8 +1,7 @@
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Col, Flex, Row, Space, Tag, Typography } from "antd";
-import { GitHubIcon, InstagramIcon, XIcon } from "../assets/icons";
-import EmailMeForm from "./EmailMeForm";
+import { Button, Col, Flex, Row, Space, Tag, Typography } from "antd";
+import { GitHubIcon, InstagramIcon, SendIcon, XIcon } from "../assets/icons";
 
 const { Title, Text } = Typography;
 function FooterContent() {
@@ -10,13 +9,17 @@ function FooterContent() {
     <>
       <Row>
         <Col span={6}>
-          <Flex vertical justify="space-between" style={{ height: "100%" }}>
-            <Flex vertical>
+          <Flex vertical justify="space-between" gap={100}>
+            <Flex vertical gap="middle">
               <Title level={3}>rebakure.com</Title>
               <Text>Let&apos;s learn and build stuff together.</Text>
+              <Button style={{ width: 150 }}>
+                <SendIcon />
+                Send me an Email
+              </Button>
             </Flex>
 
-            <Flex vertical gap="large">
+            <Flex vertical gap="middle">
               <Space size="middle" align="center">
                 <GitHubIcon style={{ fontSize: 30 }} />
                 <FontAwesomeIcon
@@ -31,29 +34,28 @@ function FooterContent() {
           </Flex>
         </Col>
         <Col span={6}>
-          <Flex vertical gap={100}>
+          <Flex vertical justify="space-between" style={{ height: "100%" }}>
             <Flex vertical gap="middle">
               <Title level={4}>My Portfolio</Title>
               <Text>My projects collection</Text>
               <Text>Contribute to my projects</Text>
             </Flex>
 
-            <Flex vertical gap="large">
-              <Title level={4}>Open to the community</Title>
-              <Space>
-                <Text>Be a core member</Text>
-                <Tag color="green">Upcoming</Tag>
-              </Space>
-              <Space>
-                <Text>Share your projects</Text>
-                <Tag color="green">Upcoming</Tag>
-              </Space>
-              <Text underline>Credits</Text>
-            </Flex>
+            <Text underline>Credits</Text>
           </Flex>
         </Col>
-        <Col span={12}>
-          <EmailMeForm />
+        <Col span={6}>
+          <Flex vertical gap="middle">
+            <Title level={4}>Open to the community</Title>
+            <Space>
+              <Text>Be a core member</Text>
+              <Tag color="green">Upcoming</Tag>
+            </Space>
+            <Space>
+              <Text>Share your projects</Text>
+              <Tag color="green">Upcoming</Tag>
+            </Space>
+          </Flex>
         </Col>
       </Row>
     </>
