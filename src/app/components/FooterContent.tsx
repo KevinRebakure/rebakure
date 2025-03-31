f735e0eee6b2b28ea714d9019e22dc37f9913891
Rebakure <kevinrebakure@gmail.com>
2025-03-31 12:43:00 +0200

    Refactor footer

--- src/app/components/FooterContent.tsx
+++ src/app/components/FooterContent.tsx
@@ -25,60 +25,56 @@ function FooterContent() {
   return (
     <>
       {screens.xs && (
-        <Row justify="center">
-          <Col span={24}>
-            <Flex vertical justify="space-between" gap={50}>
-              <Flex vertical gap="middle">
-                <Title level={3}>rebakure.com</Title>
-                <Text>Let&apos;s learn and build stuff together.</Text>
-                <Button
-                  style={{ width: 150 }}
-                  onClick={() => {
-                    copy("kevinrebakure@gmail.com");
-                    setShowEmailCopiedAlert(true);
-                    setTimeout(() => {
-                      setShowEmailCopiedAlert(false);
-                    }, 1000);
-                  }}
-                >
-                  <SendOutlined />
-                  Send me an Email
-                </Button>
-                {showEmailCopiedAlert && (
-                  <Alert
-                    message="Copied email to the clipboard!"
-                    type="success"
-                    showIcon
-                    style={{
-                      maxWidth: "max-content",
-                      position: "absolute",
-                      bottom: 155,
-                    }}
-                  />
-                )}
-              </Flex>
+        <Flex vertical justify="space-between" gap={50}>
+          <Flex vertical gap="middle">
+            <Title level={3}>rebakure.com</Title>
+            <Text>Let&apos;s learn and build stuff together.</Text>
+            <Button
+              style={{ width: 150 }}
+              onClick={() => {
+                copy("kevinrebakure@gmail.com");
+                setShowEmailCopiedAlert(true);
+                setTimeout(() => {
+                  setShowEmailCopiedAlert(false);
+                }, 1000);
+              }}
+            >
+              <SendOutlined />
+              Send me an Email
+            </Button>
+            {showEmailCopiedAlert && (
+              <Alert
+                message="Copied email to the clipboard!"
+                type="success"
+                showIcon
+                style={{
+                  maxWidth: "max-content",
+                  position: "absolute",
+                  bottom: 155,
+                }}
+              />
+            )}
+          </Flex>
 
-              <Flex vertical gap="middle">
-                <Space size="middle" align="center">
-                  <GitHubIcon style={{ fontSize: 30 }} />
-                  <FontAwesomeIcon
-                    icon={faLinkedin}
-                    style={{ width: 30, height: 30 }}
-                  />
-                  <XIcon style={{ fontSize: 30 }} />
-                  <InstagramIcon style={{ fontSize: 30 }} />
-                </Space>
-                <Text underline>&copy;2025 rebakure.com</Text>
-                <Text underline>Credits</Text>
-                <Text underline>About</Text>
-              </Flex>
-            </Flex>
-          </Col>
-        </Row>
+          <Flex vertical gap="middle">
+            <Space size="middle" align="center">
+              <GitHubIcon style={{ fontSize: 30 }} />
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                style={{ width: 30, height: 30 }}
+              />
+              <XIcon style={{ fontSize: 30 }} />
+              <InstagramIcon style={{ fontSize: 30 }} />
+            </Space>
+            <Text underline>&copy;2025 rebakure.com</Text>
+            <Text underline>Credits</Text>
+            <Text underline>About</Text>
+          </Flex>
+        </Flex>
       )}
       {screens.md && (
         <Row justify="center">
-          <Col span={6}>
+          <Col span={8}>
             <Flex vertical justify="space-between" gap={100}>
               <Flex vertical gap="middle">
                 <Title level={3}>rebakure.com</Title>
@@ -124,7 +120,7 @@ function FooterContent() {
               </Flex>
             </Flex>
           </Col>
-          <Col span={6}>
+          <Col span={8}>
             <Flex vertical justify="space-between" style={{ height: "100%" }}>
               <Flex vertical gap="middle">
                 <Title level={4}>My Portfolio</Title>
@@ -135,7 +131,7 @@ function FooterContent() {
               <Text underline>Credits</Text>
             </Flex>
           </Col>
-          <Col span={6}>
+          <Col span={8}>
             <Flex vertical gap="middle">
               <Title level={4}>Open to the community</Title>
               <Space>
