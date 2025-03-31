97a6bbde940b751d1f085aa0e87e37ac38c2b2dc
Rebakure <kevinrebakure@gmail.com>
2025-03-31 13:50:54 +0200

    Button and link text ant design component wrapper

---
+++ src/app/shared/typography/ActionText.tsx
@@ -0,0 +1,40 @@
+import { Grid, Typography } from "antd";
+
+const { Text: AntText } = Typography;
+const { useBreakpoint } = Grid;
+
+type Props = {
+  children: string;
+  style?: React.CSSProperties;
+};
+
+export default function ActionText({ children, style }: Props) {
+  const screens = useBreakpoint();
+
+  const breakpoint = screens.xxl
+    ? "xxl"
+    : screens.xl
+    ? "xl"
+    : screens.lg
+    ? "lg"
+    : screens.md
+    ? "md"
+    : screens.sm
+    ? "sm"
+    : "xs";
+
+  const fontSizes: Record<string, string> = {
+    xs: "1rem",
+    sm: "1rem",
+    md: "1rem",
+    lg: "1rem",
+    xl: "1rem",
+    xxl: "1rem",
+  };
+
+  return (
+    <AntText style={{ ...style, fontSize: fontSizes[breakpoint] }}>
+      {children}
+    </AntText>
+  );
+}
