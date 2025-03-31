3f7a75281be383cc1ef841450e99dc34eaa231e4
Rebakure <kevinrebakure@gmail.com>
2025-03-31 11:48:08 +0200

    Make footer responsive

--- src/app/components/FooterContent.tsx
+++ src/app/components/FooterContent.tsx
@@ -1,89 +1,155 @@
 import { SendOutlined } from "@ant-design/icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Alert, Button, Col, Flex, Row, Space, Tag, Typography } from "antd";
+import {
+  Alert,
+  Button,
+  Col,
+  Flex,
+  Grid,
+  Row,
+  Space,
+  Tag,
+  Typography,
+} from "antd";
 import copy from "copy-to-clipboard";
 import { useState } from "react";
 import { GitHubIcon, InstagramIcon, XIcon } from "../assets/icons";
 
 const { Title, Text } = Typography;
+const { useBreakpoint } = Grid;
 function FooterContent() {
   const [showEmailCopiedAlert, setShowEmailCopiedAlert] = useState(false);
+  const screens = useBreakpoint();
 
   return (
     <>
-      <Row>
-        <Col span={6}>
-          <Flex vertical justify="space-between" gap={100}>
-            <Flex vertical gap="middle">
-              <Title level={3}>rebakure.com</Title>
-              <Text>Let&apos;s learn and build stuff together.</Text>
-              <Button
-                style={{ width: 150 }}
-                onClick={() => {
-                  copy("kevinrebakure@gmail.com");
-                  setShowEmailCopiedAlert(true);
-                  setTimeout(() => {
-                    setShowEmailCopiedAlert(false);
-                  }, 1000);
-                }}
-              >
-                <SendOutlined />
-                Send me an Email
-              </Button>
-              {showEmailCopiedAlert && (
-                <Alert
-                  message="Copied email to the clipboard!"
-                  type="success"
-                  showIcon
-                  style={{
-                    maxWidth: "max-content",
-                    position: "absolute",
-                    bottom: 120,
+      {screens.xs && (
+        <Row justify="center">
+          <Col span={24}>
+            <Flex vertical justify="space-between" gap={50}>
+              <Flex vertical gap="middle">
+                <Title level={3}>rebakure.com</Title>
+                <Text>Let&apos;s learn and build stuff together.</Text>
+                <Button
+                  style={{ width: 150 }}
+                  onClick={() => {
+                    copy("kevinrebakure@gmail.com");
+                    setShowEmailCopiedAlert(true);
+                    setTimeout(() => {
+                      setShowEmailCopiedAlert(false);
+                    }, 1000);
                   }}
-                />
-              )}
-            </Flex>
+                >
+                  <SendOutlined />
+                  Send me an Email
+                </Button>
+                {showEmailCopiedAlert && (
+                  <Alert
+                    message="Copied email to the clipboard!"
+                    type="success"
+                    showIcon
+                    style={{
+                      maxWidth: "max-content",
+                      position: "absolute",
+                      bottom: 155,
+                    }}
+                  />
+                )}
+              </Flex>
 
+              <Flex vertical gap="middle">
+                <Space size="middle" align="center">
+                  <GitHubIcon style={{ fontSize: 30 }} />
+                  <FontAwesomeIcon
+                    icon={faLinkedin}
+                    style={{ width: 30, height: 30 }}
+                  />
+                  <XIcon style={{ fontSize: 30 }} />
+                  <InstagramIcon style={{ fontSize: 30 }} />
+                </Space>
+                <Text underline>&copy;2025 rebakure.com</Text>
+                <Text underline>Credits</Text>
+                <Text underline>About</Text>
+              </Flex>
+            </Flex>
+          </Col>
+        </Row>
+      )}
+      {screens.md && (
+        <Row justify="center">
+          <Col span={6}>
+            <Flex vertical justify="space-between" gap={100}>
+              <Flex vertical gap="middle">
+                <Title level={3}>rebakure.com</Title>
+                <Text>Let&apos;s learn and build stuff together.</Text>
+                <Button
+                  style={{ width: 150 }}
+                  onClick={() => {
+                    copy("kevinrebakure@gmail.com");
+                    setShowEmailCopiedAlert(true);
+                    setTimeout(() => {
+                      setShowEmailCopiedAlert(false);
+                    }, 1000);
+                  }}
+                >
+                  <SendOutlined />
+                  Send me an Email
+                </Button>
+                {showEmailCopiedAlert && (
+                  <Alert
+                    message="Copied email to the clipboard!"
+                    type="success"
+                    showIcon
+                    style={{
+                      maxWidth: "max-content",
+                      position: "absolute",
+                      bottom: 120,
+                    }}
+                  />
+                )}
+              </Flex>
+
+              <Flex vertical gap="middle">
+                <Space size="middle" align="center">
+                  <GitHubIcon style={{ fontSize: 30 }} />
+                  <FontAwesomeIcon
+                    icon={faLinkedin}
+                    style={{ width: 30, height: 30 }}
+                  />
+                  <XIcon style={{ fontSize: 30 }} />
+                  <InstagramIcon style={{ fontSize: 30 }} />
+                </Space>
+                <Text underline>&copy;2025 rebakure.com</Text>
+              </Flex>
+            </Flex>
+          </Col>
+          <Col span={6}>
+            <Flex vertical justify="space-between" style={{ height: "100%" }}>
+              <Flex vertical gap="middle">
+                <Title level={4}>My Portfolio</Title>
+                <Text>My projects collection</Text>
+                <Text>Contribute to my projects</Text>
+              </Flex>
+
+              <Text underline>Credits</Text>
+            </Flex>
+          </Col>
+          <Col span={6}>
             <Flex vertical gap="middle">
-              <Space size="middle" align="center">
-                <GitHubIcon style={{ fontSize: 30 }} />
-                <FontAwesomeIcon
-                  icon={faLinkedin}
-                  style={{ width: 30, height: 30 }}
-                />
-                <XIcon style={{ fontSize: 30 }} />
-                <InstagramIcon style={{ fontSize: 30 }} />
+              <Title level={4}>Open to the community</Title>
+              <Space>
+                <Text>Be a core member</Text>
+                <Tag color="green">Upcoming</Tag>
               </Space>
-              <Text underline>&copy;2025 rebakure.com</Text>
+              <Space>
+                <Text>Share your projects</Text>
+                <Tag color="green">Upcoming</Tag>
+              </Space>
             </Flex>
-          </Flex>
-        </Col>
-        <Col span={6}>
-          <Flex vertical justify="space-between" style={{ height: "100%" }}>
-            <Flex vertical gap="middle">
-              <Title level={4}>My Portfolio</Title>
-              <Text>My projects collection</Text>
-              <Text>Contribute to my projects</Text>
-            </Flex>
-
-            <Text underline>Credits</Text>
-          </Flex>
-        </Col>
-        <Col span={6}>
-          <Flex vertical gap="middle">
-            <Title level={4}>Open to the community</Title>
-            <Space>
-              <Text>Be a core member</Text>
-              <Tag color="green">Upcoming</Tag>
-            </Space>
-            <Space>
-              <Text>Share your projects</Text>
-              <Tag color="green">Upcoming</Tag>
-            </Space>
-          </Flex>
-        </Col>
-      </Row>
+          </Col>
+        </Row>
+      )}
     </>
   );
 }
