6972c8989a46c2af65c486872547e5bfc29e8b5c
Rebakure <kevinrebakure@gmail.com>
2025-03-31 13:50:01 +0200

    Export custom text wrapper components

---
+++ src/app/shared/typography/index.ts
@@ -0,0 +1,9 @@
+import Title from "./Title";
+import H1 from "./H1";
+import H2 from "./H2";
+import H3 from "./H3";
+import H4 from "./H4";
+import Text from "./Text";
+import ActionText from "./ActionText";
+
+export { Title, H1, H2, H3, H4, Text, ActionText };
