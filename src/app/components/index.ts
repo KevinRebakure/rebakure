2e5af6fa5829a25cb53dadbd1fb6092a3b237a58
Rebakure <kevinrebakure@gmail.com>
2025-03-31 12:43:16 +0200

    Export landing page components in a barrel file

---
+++ src/app/components/index.ts
@@ -0,0 +1,7 @@
+import FooterContent from "./FooterContent";
+import Motto from "./Motto";
+import Portals from "./Portals";
+import SkillSet from "./SkillSet";
+import Welcome from "./Welcome";
+
+export { FooterContent, Motto, Portals, SkillSet, Welcome };
